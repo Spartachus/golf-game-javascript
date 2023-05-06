232b16cf77557611a5d2fefc5a43cd8fda93e783
Imaadu Deen Tawiah <106468069+Spartachus@users.noreply.github.com>
2023-05-06 11:39:55 +0200

    Create golf.js

---
+++ golf.js
@@ -0,0 +1,15 @@
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if (strokes == 1){console.log(names[0])}
+  else if (strokes <= par - 2	){console.log(names[1])}
+  else if (strokes == par - 1){console.log(names[2])}
+  else if (strokes == par + 1){console.log(names[4])}
+  else if (strokes  == par + 2){console.log(names[5])}
+  else if (strokes >= par + 3 ){console.log(names[6])}
+  else if (strokes  == par){console.log(names[3])};
+
+}
+
+golfScore(5, 4);
